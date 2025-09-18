a022b59490d269a64bae25063253105c7c27ae3c
Calvin Bulthuis <calvinbulthuis@Calvins-MacBook-Air.local>
2025-09-18 07:34:22 -0700

    Adding text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <main class="bg-yellow-200 min-h-screen p-6">
-	<h1>Welcome to SvelteKit</h1>
+	<h1>Welcome to Calvin's very boring website</h1>
 	<p>Visit <a href="https://svelte.dev/docs/kit" class="text-red-600 link">svelte.dev/docs/kit</a> to read the documentation</p>
 </main>
